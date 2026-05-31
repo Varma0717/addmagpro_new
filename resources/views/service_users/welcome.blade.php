2010ac06c1d4a672c7a9b11146528322bb932564
Varma0717 <bhuvaneshbunty1999@gmail.com>
2026-05-31 12:53:31 +0530

    Fix: Remove hero section top margin to eliminate header gap

--- resources/views/service_users/welcome.blade.php
+++ resources/views/service_users/welcome.blade.php
@@ -173,7 +173,7 @@
 @section('mainsection')
 
 {{-- ====== 1. HERO SWIPER BANNER ====== --}}
-<section class="hero-swiper pb-7">
+<section class="hero-swiper pb-7 !mt-0">
     <div class="swiper hero-slider" id="heroSlider">
         <div class="swiper-wrapper">
             @isset($main_slider_banner)
